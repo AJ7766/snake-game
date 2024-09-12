8121754ab4a34cea4177134f998eb87c07dd0638
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-09-12 12:46:16 +0200

    removed one of the updatelist functions because I could fit allt he logic in the other one

--- src/_actions/userListActions.ts
+++ src/_actions/userListActions.ts
@@ -2,30 +2,31 @@ import { useUserContext } from "../../context/UserContext";
 import { ExcelRow } from "../excel-dropzone";
 import { InitialDataProps, UserObjectProps } from "../types/types";
 
-const getInitialData = ({ users, scores }: InitialDataProps): Map<string, UserObjectProps> => {
-    const initialUserList = new Map<string, UserObjectProps>();
+const getInitialData = ({ users, scores }: InitialDataProps): UserObjectProps[]=> {
+    const initialUserMap = new Map<string, UserObjectProps>();
 
     users.forEach(user => {
-        initialUserList.set(user.name, { name: user.name, scores: [] });
+        initialUserMap.set(user.name, { name: user.name, scores: [] });
     });
 
     scores.forEach(score => {
         const user = users.find(user_ => user_._id === score.userId);
         if (user) {
-            const existingUser = initialUserList.get(user.name);
+            const existingUser = initialUserMap.get(user.name);
             existingUser && existingUser.scores.push(score.score);
         }
     });
-    
-    return initialUserList;
+    const updatedUserList: UserObjectProps[] = Array.from(initialUserMap.values());
+
+    return updatedUserList;
 };
 
-const convertExcelToUserObjects = (excelUsers: ExcelRow[]): Map<string, UserObjectProps> => {
-    const convertedExcelList = new Map<string, UserObjectProps>();
+const convertExcelToUserObjects = (excelUsers: ExcelRow[]): UserObjectProps[] => {
+    const convertedExcelMap = new Map<string, UserObjectProps>();
 
     excelUsers.forEach((user) => {
-        if (convertedExcelList.has(user.name)) {
-            const existingUser = convertedExcelList.get(user.name);
+        if (convertedExcelMap.has(user.name)) {
+            const existingUser = convertedExcelMap.get(user.name);
             if (existingUser) {
                 existingUser.scores.push(user.score);
             }
@@ -34,23 +35,23 @@ const convertExcelToUserObjects = (excelUsers: ExcelRow[]): Map<string, UserObje
                 name: user.name,
                 scores: [user.score]
             };
-            convertedExcelList.set(user.name, newUser);
+            convertedExcelMap.set(user.name, newUser);
         }
     });
+    const convertedExcelList: UserObjectProps[] = Array.from(convertedExcelMap.values());
+
     return convertedExcelList;
 };
 
-const updateUserList = (
-    prevUserList: UserObjectProps[],
-    newUserList: Map<string, UserObjectProps>
-): UserObjectProps[] => {
+const mergeAndUpdateUserList = (mergedList: UserObjectProps[]): UserObjectProps[] => 
+    {
     const currentUserList = new Map<string, UserObjectProps>();
 
-    prevUserList.forEach(user => {
+    mergedList.forEach(user => {
         currentUserList.set(user.name, { ...user });
     });
 
-    newUserList.forEach((newUser) => {
+    mergedList.forEach((newUser) => {
         if (currentUserList.has(newUser.name)) {
             const existingUser = currentUserList.get(newUser.name);
             if (existingUser) {
@@ -74,52 +75,21 @@ const updateUserList = (
     return updatedUserList;
 };
 
-const mergeAndSortUserList = (newUserList: UserObjectProps[]): UserObjectProps[] => {
-    const userMap = new Map<string, UserObjectProps>();
-    
-    newUserList.forEach(newUser => {
-        if (userMap.has(newUser.name)) {
-            const existingUser = userMap.get(newUser.name);
-            if (existingUser) {
-                userMap.set(newUser.name, {
-                    ...existingUser,
-                    scores: Array.from(new Set([...existingUser.scores, ...newUser.scores])),
-                });
-            }
-        } else {
-            userMap.set(newUser.name, { ...newUser });
-        }
-    });
-    const mergedList = Array.from(userMap.values());
-
-    mergedList.forEach(user => {
-        user.scores.sort((a, b) => b - a);
-    });
-
-    mergedList.sort((a, b) => {
-        const highestScoreA = a.scores[0] || 0;
-        const highestScoreB = b.scores[0] || 0;
-        return highestScoreB - highestScoreA;
-    });
-
-    return mergedList;
-};
-
 export function useUserListActions() {
     const { setUserList } = useUserContext();
 
     const handleInitialData = (data: InitialDataProps) => {
         const newUserList = getInitialData(data);
-        setUserList(prevList => updateUserList(prevList, newUserList));
+        setUserList(prevList => mergeAndUpdateUserList([...prevList, ...newUserList]));
     };
 
     const handleExcelConversion = (excelUsers: ExcelRow[]) => {
         const newUserList = convertExcelToUserObjects(excelUsers);
-        setUserList(prevList => updateUserList(prevList, newUserList));
+        setUserList(prevList => mergeAndUpdateUserList([...prevList, ...newUserList]));
     };
 
     const handleUserListUpdate = (newUsers: UserObjectProps[]) => {
-        setUserList(prevList => mergeAndSortUserList([...prevList, ...newUsers]));
+        setUserList(prevList => mergeAndUpdateUserList([...prevList, ...newUsers]));
     };
 
     return { handleInitialData, handleExcelConversion, handleUserListUpdate };
